6a411c49c2761f1649b69f4a5a1553a954a9980d
Simon Scatton <simon.scatton@epita.fr>
2020-09-28 12:42:03 +0200

    Install wrapper and actions

--- src/main/java/fr/thenaturlfoundation/idl/MainApp.java
+++ src/main/java/fr/thenaturlfoundation/idl/MainApp.java
@@ -1,9 +1,19 @@
+/*
+ * Copyright (C) The NaturL Foundation - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by simon, 28/9/2020
+ */
+
 package fr.thenaturlfoundation.idl;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
 import javafx.stage.Stage;
 
 
@@ -11,14 +21,11 @@ public class MainApp extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-
         FXMLLoader loader = new FXMLLoader();
         loader.setLocation(getClass().getResource("/scene.fxml"));
         Parent root = loader.load();
-
         Scene scene = new Scene(root);
         scene.getStylesheets().add(getClass().getResource("/styles.css").toExternalForm());
-
         stage.setTitle("idL for naturL");
         stage.setScene(scene);
         stage.show();
--- src/main/java/fr/thenaturlfoundation/idl/MainController.java
+++ src/main/java/fr/thenaturlfoundation/idl/MainController.java
@@ -3,13 +3,28 @@ package fr.thenaturlfoundation.idl;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
 
 public class MainController implements Initializable {
+    @FXML
+    TabPane tabPane;
+
+    @FXML
+    MenuItem openMenuButton;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+        System.out.println("Hi there!");
         String javaVersion = System.getProperty("java.version");
         String javafxVersion = System.getProperty("javafx.version");
+        openMenuButton.setOnAction(event -> {
+            tabPane.getTabs().get(0).setText("Hello World!");
+            System.out.println("Hi there!");
+        });
     }
 }
