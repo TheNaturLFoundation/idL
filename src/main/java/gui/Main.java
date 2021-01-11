e2a3255f665caf782918192041a1e5b6f2fe59a5
Simon Scatton <simon.scatton@epita.fr>
2021-01-11 12:16:00 +0100

    Removed some copyright headers, added javadoc generation to the dev scrip

--- src/main/java/gui/Main.java
+++ src/main/java/gui/Main.java
@@ -5,12 +5,7 @@
  * Written by simon, 11/1/2021
  */
 
-package gui;/*
- * Copyright (C) The NaturL Foundation - All Rights Reserved
- * Unauthorized copying of this file, via any medium is strictly prohibited
- * Proprietary and confidential
- * Written by simon, 28/9/2020
- */
+package gui;
 
 public class Main {
     public static void main(String[] args) {
